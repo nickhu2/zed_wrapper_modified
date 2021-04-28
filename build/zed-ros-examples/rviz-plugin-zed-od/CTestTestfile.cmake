eb680ae426f41554544529f6cc53510978687940
huwx <huwx12@163.com>
2021-04-28 16:48:06 +0800

    1st patch in zed wrapper

---
+++ build/zed-ros-examples/rviz-plugin-zed-od/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws/src/zed-ros-examples/rviz-plugin-zed-od
+# Build directory: /home/ubuntu/catkin_ws/build/zed-ros-examples/rviz-plugin-zed-od
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/zed-ros-wrapper/zed_interfaces/CMakeFiles/zed_interfaces_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,32 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zed_interfaces_generate_messages_lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/msg/BoundingBox2Di.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/msg/Keypoint3D.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/msg/Skeleton2D.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/msg/Keypoint2Di.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/msg/Object.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/msg/Keypoint2Df.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/msg/BoundingBox2Df.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/msg/BoundingBox3D.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/msg/ObjectsStamped.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/msg/RGBDSensors.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/msg/Skeleton3D.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/reset_odometry.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/reset_tracking.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/toggle_led.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/stop_svo_recording.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/start_3d_mapping.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/set_led_status.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/stop_object_detection.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/start_object_detection.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/start_remote_stream.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/stop_3d_mapping.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/stop_remote_stream.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/start_svo_recording.lisp"
+  "/home/ubuntu/catkin_ws/devel/share/common-lisp/ros/zed_interfaces/srv/set_pose.lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/zed_interfaces_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
