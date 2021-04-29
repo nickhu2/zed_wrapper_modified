dae97137b2d50efed365da622f5b54e1d5b4fe42
huwenxin02 <huwenxin02@meituan.com>
2021-04-29 16:25:18 +0800

    1st patch

--- build/zed-ros-examples/examples/zed_rtabmap_example/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/catkin_ws/src/zed-ros-examples/examples/zed_rtabmap_example
-# Build directory: /home/ubuntu/catkin_ws/build/zed-ros-examples/examples/zed_rtabmap_example
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/zed-ros-examples/tests/zed_sync_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/catkin_ws/src/zed-ros-examples/tests/zed_sync_test
-# Build directory: /home/ubuntu/catkin_ws/build/zed-ros-examples/tests/zed_sync_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
